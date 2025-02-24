fcdca650d1349038c4e3e546fa4b8dbcf0e641ad
evlx <evilnyagaming@yandex.ru>
2025-02-24 21:12:46 +0300

    добавлены вспомогательные функции add_packet_to_dict, process_icmp, process_arp. Убраны повторяющиеся куски кода

--- pcap_analize.py
+++ pcap_analize.py
@@ -2,13 +2,50 @@ from scapy.all import rdpcap, Ether, IP, ICMP, ARP
 from datetime import datetime
 
 
+def add_packet_to_dict(packet_dict, key, packet):
+    """Добавляет пакет в словарь, если ключ еще не существует."""
+    if key not in packet_dict:
+        packet_dict[key] = []
+    packet_dict[key].append(packet)
+
+
+def process_icmp(packet, mac_src, mac_dst):
+    """Обрабатывает ICMP пакеты и выводит информацию о них."""
+    icmp_type = packet[ICMP].type
+    icmp_id = packet[ICMP].id
+    icmp_seq = packet[ICMP].seq
+    timestamp = float(packet.time)
+    dt_object = datetime.fromtimestamp(timestamp)
+
+    if icmp_type == 8:  # Echo request
+        print(
+            f"ICMP Запрос: {
+                packet[IP].src} -> {
+                packet[IP].dst}, ID={icmp_id}, Seq={icmp_seq}, Время={dt_object}")
+        return (packet[IP].src, mac_src, mac_dst, dt_object, icmp_id, icmp_seq)
+    elif icmp_type == 0:  # Echo reply
+        print(
+            f"ICMP Ответ: {
+                packet[IP].src} -> {
+                packet[IP].dst}, ID={icmp_id}, Seq={icmp_seq}, Время={dt_object}")
+        return (packet[IP].src, mac_src, mac_dst, dt_object, icmp_id, icmp_seq)
+
+
+def process_arp(packet):
+    """Обрабатывает ARP пакеты и выводит информацию о них."""
+    if packet[ARP].op == 1:  # ARP request
+        print(f"ARP Запрос: {packet[IP].src} ищет {packet[IP].dst}")
+    elif packet[ARP].op == 2:  # ARP reply
+        print(f"ARP Ответ: {packet[IP].src} отвечает {packet[IP].dst}")
+
+
 def analyze_pcap(file_path):
     packets = rdpcap(file_path)
+
     icmp_requests = []
     icmp_replies = []
     arp_requests = []
     arp_replies = []
-    other_packets = []
     mac_dict = {}
     ip_dict = {}
 
@@ -16,60 +53,27 @@ def analyze_pcap(file_path):
         if Ether in packet:
             eth_src = packet[Ether].src
             eth_dst = packet[Ether].dst
-
-            if eth_src not in mac_dict:
-                mac_dict[eth_src] = []
-            mac_dict[eth_src].append(packet)
-
-            if eth_dst not in mac_dict:
-                mac_dict[eth_dst] = []
-            mac_dict[eth_dst].append(packet)
+            add_packet_to_dict(mac_dict, eth_src, packet)
+            add_packet_to_dict(mac_dict, eth_dst, packet)
 
         if IP in packet:
             ip_src = packet[IP].src
             ip_dst = packet[IP].dst
+            add_packet_to_dict(ip_dict, ip_src, packet)
+            add_packet_to_dict(ip_dict, ip_dst, packet)
 
-            if ip_src not in ip_dict:
-                ip_dict[ip_src] = []
-            ip_dict[ip_src].append(packet)
-
-            if ip_dst not in ip_dict:
-                ip_dict[ip_dst] = []
-            ip_dict[ip_dst].append(packet)
-
+            # Обработка ICMP пакетов
             if ICMP in packet:
-                icmp_type = packet[ICMP].type
-                icmp_id = packet[ICMP].id
-                icmp_seq = packet[ICMP].seq
+                result = process_icmp(packet, eth_src, eth_dst)
+                if result:
+                    if result in icmp_requests:
+                        icmp_replies.append(result)
+                    else:
+                        icmp_requests.append(result)
 
-                if icmp_type == 8:  # Echo request
-                    timestamp = int(packet.time)
-                    dt_object = datetime.fromtimestamp(timestamp)
-                    icmp_requests.append(
-                        (ip_src, eth_src, eth_dst, dt_object, icmp_id, icmp_seq))
-                    print(
-                        f"ICMP Запрос: {ip_src} -> {ip_dst}, ID={icmp_id}, Seq={icmp_seq}, Время={dt_object}")
-
-                elif icmp_type == 0:  # Echo reply
-                    timestamp = packet.time
-                    dt_object = datetime.fromtimestamp(timestamp)
-                    icmp_replies.append(
-                        (ip_src, eth_src, eth_dst, dt_object, icmp_id, icmp_seq))
-                    print(
-                        f"ICMP Ответ: {ip_src} -> {ip_dst}, ID={icmp_id}, Seq={icmp_seq}, Время={dt_object}")
-
-                else:
-                    other_packets.append(packet)
-
+            # Обработка ARP пакетов
             elif ARP in packet:
-                if packet[ARP].op == 1:
-                    arp_requests.append((ip_src, eth_src, eth_dst))
-                    print(f"ARP Запрос: {ip_src} ищет {ip_dst}")
-                elif packet[ARP].op == 2:
-                    arp_replies.append((ip_src, eth_src, eth_dst))
-                    print(f"ARP Ответ: {ip_src} отвечает {ip_dst}")
-            else:
-                other_packets.append(packet)
+                process_arp(packet)
 
     # Проверка отсутствующих ответов на ICMP
     request_ids = {(req[4], req[5]): req for req in icmp_requests}
@@ -86,40 +90,29 @@ def analyze_pcap(file_path):
                     req[1]}), отправленный в {
                     req[3]}")
 
-    # Анализ обнаруженных ARP сообщений
-    print("\nСтатистика ARP:")
-    print(f"Запросы ARP: {len(arp_requests)}, Ответы ARP: {len(arp_replies)}")
-
     # Подсчет всех пакетов в файле
+    print(f"\nОбщее количество пакетов в файле: {len(packets)}")
     print(
-        f"Общее количество пакетов в файле: {
-            len(packets)} (ICMP пакеты: {
-            len(icmp_requests) +
-            len(icmp_replies)}, ARP пакеты: {
-                len(arp_requests) +
-                len(arp_replies)}, другие пакеты: {
-                    len(other_packets)}")
+        f"ICMP пакеты: {
+            len(icmp_requests)}, ARP пакеты: {
+            len(arp_requests)}, другие пакеты: {
+                len(packets) -
+                len(icmp_requests) -
+            len(arp_requests)}")
 
     # Анализ MAC-адресов
     print("\nАнализ MAC-адресов:")
     for mac, packets in mac_dict.items():
         print(f"MAC: {mac}")
         for packet in packets:
-            print(f"  Packet: {packet}")
+            print(f"  Пакет: {packet.summary()}")
 
     # Анализ IP-адресов
     print("\nАнализ IP-адресов:")
     for ip, packets in ip_dict.items():
         print(f"IP: {ip}")
         for packet in packets:
-            print(f"  Packet: {packet}")
-
-    # Анализ времени отправки и получения пакетов
-    print("\nАнализ времени отправки и получения пакетов:")
-    for packet in packets:
-        timestamp = int(packet.time)
-        dt_object = datetime.fromtimestamp(timestamp)
-        print(f"Packet: {packet}, Время: {dt_object}")
+            print(f"  Пакет: {packet.summary()}")
 
 
 if __name__ == "__main__":
